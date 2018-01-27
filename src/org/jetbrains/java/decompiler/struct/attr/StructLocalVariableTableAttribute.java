e4c05fb3da069a6901af378d173f1917067fed15
Renz Bagaporo <renz.bagaporo@gmail.com>
2018-01-27 12:19:14 +0800

    Convert more lambdas into anonymous functions/class

--- src/org/jetbrains/java/decompiler/struct/attr/StructLocalVariableTableAttribute.java
+++ src/org/jetbrains/java/decompiler/struct/attr/StructLocalVariableTableAttribute.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -93,7 +94,27 @@ public class StructLocalVariableTableAttribute extends StructGeneralAttribute {
   }
 
   public Map<Integer, String> getMapParamNames() {
-    return localVariables.stream().filter(v -> v.start_pc == 0).collect(Collectors.toMap(v -> v.index, v -> v.name, (n1, n2) -> n2));
+    return localVariables.stream().filter(new Predicate<LocalVariable>() {
+		@Override
+		public boolean test(LocalVariable v) {
+			return v.start_pc == 0;
+		}
+	}).collect(Collectors.toMap(new Function<LocalVariable, Integer>() {
+		@Override
+		public Integer apply(LocalVariable v) {
+			return v.index;
+		}
+	}, new Function<LocalVariable, String>() {
+		@Override
+		public String apply(LocalVariable v) {
+			return v.name;
+		}
+	}, new BinaryOperator<String>() {
+		@Override
+		public String apply(String n1, String n2) {
+			return n2;
+		}
+	}));
   }
 
   private static class LocalVariable {
